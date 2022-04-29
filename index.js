22a946e9826584bf8686bff35041498484772cae
destu3 <obose03@gmail.com>
2022-04-30 00:28:39 +0100

    added victory message

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let playerScore = 0;
 let computerScore= 0;
 
+game();
+
 function computerPlay(){
     const options = ["Rock","Paper","Scissors"];
     const randomIndex = Math.round(Math.random() * 2);
@@ -15,7 +17,6 @@ function playerChoice(){
 function playRound(computerSelection, playerSelection){
 
     let result;
-  
     if(computerSelection == playerSelection)
     {
         result = `No one won this round, computer chose ${computerSelection} and you chose ${playerSelection}`
@@ -59,11 +60,16 @@ function game(){
         console.log(playRound(computerPlay(),playerChoice()));
         if(i === 4){
             console.log(`Final results are: You scored ${playerScore},  Computer scored ${computerScore}`);
+            
+            if(computerScore > playerScore){
+                console.log("The computer remains Triumphant")
+            }
+            else if(computerScore < playerScore){
+                console.log("Man-kind has defeated technology")
+            }
+            else{
+                console.log("Mankind and computers are evenly matched ... apparently ")
+            }
         }
      }
 }
-
-game();
-
-
-

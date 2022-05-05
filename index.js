fb806b4e0730b99ea35f99b376062182debe2c38
destu3 <obose03@gmail.com>
2022-05-05 16:34:51 +0100

    added results tracking and winner func

--- index.js
+++ index.js
@@ -13,8 +13,12 @@ Plan: GAME STARTS
 
 let playerScore = 0;
 let computerScore = 0;
-const choiceButtons = document.querySelectorAll(".choiceButton")
-const roundResults = document.querySelector(".roundResults")
+const choiceButtons = document.querySelectorAll(".choiceButton");
+const roundResults = document.querySelector(".roundResults");
+const playerScoreDiv = document.querySelector(".playerScore");
+const computerScoreDiv = document.querySelector(".computerScore");
+const winnerSect = document.querySelector(".overallWinner"); 
+let winnerText = document.createElement("h3");
 
 //returns/generates random choice value for the computer
 function computerChoice(){
@@ -70,43 +74,43 @@ function playRound(computerSelection, playerSelection){
     return result;
 }
 
-//main game function. 5 rounds are played
-// function game(){
-//     for (let i = 1; i <= 5; i++) {
-//         console.log(playRound(computerPlay(),playerChoice()));
-//         if(i === 5){
-//             console.log(`Final results are: You scored ${playerScore} and the Computer scored ${computerScore}`);
-            
-//             if(computerScore > playerScore){
-//                 console.log("The computer takes the W")
-//             }
-//             else if(computerScore < playerScore){
-//                 console.log("Man-kind has defeated technology")
-//             }
-//             else{
-//                 console.log("Mankind and computers are evenly matched ... apparently ")
-//             }
-//         }
-//      }
-// }
 
-
-// for each button of class choiceButton run add an event listener to it and determine what button was clicked and set playerChoice accordingly
+    
+// for each button of class choiceButton run add an event listener to it and determine what button was clicked and set playerChoice accordingly    
 choiceButtons.forEach((choiceButton) => {
     choiceButton.addEventListener("click", (e) => {
         let playerChoice = "";
         if (e.currentTarget.classList.contains("rockButton")){
-            playerChoice = "Rock";
-        }
+                playerChoice = "Rock";
+            }
         else if (e.currentTarget.classList.contains("scissorsButton")){
-            playerChoice = "Scissors";
-        }
-        else{
-            playerChoice = "Paper";
-        }
-        roundResults.textContent = playRound(computerChoice(),playerChoice)
+                playerChoice = "Scissors";
+            }
+            else{
+                playerChoice = "Paper";
+            }
+            roundResults.textContent = playRound(computerChoice(),playerChoice)
+            
+            playerScoreDiv.textContent = `Player: ${playerScore}`;
+            computerScoreDiv.textContent = `Computer: ${computerScore}`;
+
+        if (playerScore === 5){
+            
+            winnerText.textContent = "The Player is victorious, Every little victory counts"
+            winnerSect.append(winnerText);
+                choiceButtons.forEach(choiceButton => {
+                    choiceButton.disabled = true;
+                });
+            }
+        else if(computerScore === 5){
+            winnerText.textContent = "The Computer is victorious, The will to conquer is the first condition of victory."
+            winnerSect.append(winnerText);
+               choiceButtons.forEach(choiceButton => {
+                    choiceButton.disabled = true;
+                });
+            }   
     })
 })
 
 
-//game();
+
